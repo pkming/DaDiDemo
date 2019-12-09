a5ae4157acbc10279cf53772d072e24d668c8b0f
pkming <707208214@qq.com>
2019-12-09 10:06:54 +0800

    Initial Commit

---
+++ DaDiTopApps-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DaDiTopApps-Demo
+//
+//  Created by 夏以铭 on 2019/12/9.
+//  Copyright © 2019 夏以铭. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DaDiTopApps-DemoTests/DaDiTopApps_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  DaDiTopApps_DemoTests.swift
+//  DaDiTopApps-DemoTests
+//
+//  Created by 夏以铭 on 2019/12/9.
+//  Copyright © 2019 夏以铭. All rights reserved.
+//
+
+import XCTest
+@testable import DaDiTopApps_Demo
+
+class DaDiTopApps_DemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
